3080cb87456a8fdf8ba745b32113e5b207bce68c
deepu <sandeep.ramachandran@bs-shipmanagement.com>
2013-10-21 14:34:24 +0530

    hide js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,19 @@
 var Movies = {
 	savemovie: function() {
 		jsRoutes.controllers.MoviesController.savemovies().ajax({
+			/*
 			data : data,
-			action : "POST",
-			success: function(data,status) {
-				$("#addRow").remove();
-				$("#detTable").append(data);
-			},
-			error : function(data){
-				var json = data.responseText;
-			    obj = JSON.parse(json);
-				alert(obj.name);
-			}
+						action : "POST",
+						success: function(data,status) {
+							$("#addRow").remove();
+							$("#detTable").append(data);
+						},
+						error : function(data){
+							var json = data.responseText;
+							obj = JSON.parse(json);
+							alert(obj.name);
+						}*/
+			
 		});
 
 	}
